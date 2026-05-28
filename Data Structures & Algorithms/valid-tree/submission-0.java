bf1668e9aa89e42f6b4ad066434f5a9f708b42a0
Upasana Ghosh <upasana.ghosh30@gmail.com>
2026-05-28 12:03:22 -0700

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // tree with n nodes can have only n-1 edges
+        if(edges.length > n - 1) return false; 
+
+        List<Integer>[] adjList = new List[n];
+        Set<Integer> vis = new HashSet<>();
+
+        for(int i=0; i < n; i++){
+            adjList[i] = new ArrayList<>();
+        }
+
+        for(int[] e: edges){
+            adjList[e[0]].add(e[1]);
+            adjList[e[1]].add(e[0]);
+        }
+
+        Queue<int[]> bfsQ = new ArrayDeque<>();
+        bfsQ.offer(new int[]{0, -1}); // [node, parent]
+        vis.add(0);
+
+        while(!bfsQ.isEmpty()){
+            int len = bfsQ.size();
+            
+            for(int i=0; i < len; i++){
+                int[] elem = bfsQ.poll();
+                int curr = elem[0];
+                int parent = elem[1];
+
+                for(int neigh: adjList[curr]){
+                    if(neigh == parent) continue;
+                    if(vis.contains(neigh)) return false;
+
+                    bfsQ.offer(new int[]{neigh, curr});
+                    vis.add(neigh);
+                }
+            }
+        }
+
+        return vis.size() == n;
+    }
+}
+/*
+0 - 1
+1 - 0,2,3,4
+2 - 3
+3 -
+4 - 
+*/
